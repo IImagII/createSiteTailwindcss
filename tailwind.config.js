78331094c934a759eaa23680328182c87b4d1abe
iimagii <mag_1@ukr.net>
2022-07-31 22:57:04 +0300

    add two sections at the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,10 @@ module.exports = {
             lightgray: '#747474',
             darkgray: '#272727',
          },
+         boxShadow: {
+            customred: '0px 0px 30px rgba(255, 13, 56, 0.21)',
+         },
       },
    },
    plugins: [],
-};
+}
